f4db2f23f5a3b637b5d7eb7e2710db98a70ddcf5
Parkerbot111 <jeanparker921@gmail.com>
2024-08-28 07:21:58 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 const code = require('./pair');
 
-// Set the maximum number of listeners to 500 (optional)
-require('events').EventEmitter.defaultMaxListeners = 500;
-
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -29,7 +26,7 @@ app.use((err, req, res, next) => {
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`YouTube: https://www.youtube.com/@Jean-Parker-tech\nTelegram: t.me/Jeanparker100\nGitHub: https://github.com/Jeanparker100\nInstagram: https://www.instagram.com/its_jeanparker?igsh=MXF3OHgzcnJsZzhqNQ==\n\nServer running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
 
 module.exports = app;
